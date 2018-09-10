568a99238938617f9ba633c962c0ec50c853ee27
dvdrtrgn <dvdrtrgn@gmail.com>
2018-09-10 13:09:09 -0500

    Animate tooth chart

--- src/main.js
+++ src/main.js
@@ -27,21 +27,43 @@ setTimeout(function() {
   body.classList.add(myVue._route.name);
 }, 99);
 
-// window.console.log(KsVueScrollmagic, myVue);
+window.console.log(myVue);
 
 function aniThis() {
   var con = new myVue.$scrollmagic.Controller();
   var sce = new myVue.$scrollmagic.Scene({
-    triggerElement: this,
     offset: -222,
     reverse: false,
+    triggerElement: this,
   });
 
   sce.setClassToggle(this, 'ani').addTo(con);
 }
 
+function aniChart(num) {
+  var con = new myVue.$scrollmagic.Controller();
+  var ele = '.tooth .chart svg .line-fill';
+  var def = {
+    scene: {
+      offset: -111,
+      reverse: false,
+      triggerElement: ele,
+    },
+    tween: {
+      delay: 1,
+      ease: myVue.$gsap.Bounce.easeOut,
+      strokeDashoffset: num,
+    },
+  };
+
+  def.scene = new myVue.$scrollmagic.Scene(def.scene);
+  def.tween = myVue.$gsap.TweenMax.to(ele, 1, def.tween);
+  con.addScene(def.scene.setTween(def.tween));
+}
+
 function ani() {
   $('section').each(aniThis);
+  aniChart(250);
 }
 
 window.setTimeout(ani, 999);
